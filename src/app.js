f21f3b8b00a5c368736cc4b4db9a4110576735c8
Aman Kumar <amankr4604@gmail.com>
2026-04-24 20:01:57 +0530

    add home route

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ const transactionRouter = require("./routes/transaction.routes")
 /**
  * - Use Routes
  */
+
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
+
 app.use("/api/auth", authRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/transactions", transactionRouter)
